9be4e980a4e888bb6205d195eb7a1e7d987a29db
rafafhorta <rafaelafhorta@gmail.com>
2021-07-07 21:30:33 -0300

    Criação do método construtor do Client.

--- src/main/java/ufjf/dcc025/trabalho/controller/Client.java
+++ src/main/java/ufjf/dcc025/trabalho/controller/Client.java
@@ -11,4 +11,15 @@ public class Client {
     Address address;
     String birthDate;
     String password;
+
+    public Client(String name, String email, String cpf, String rg, Address address, String birthDate, String password){
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.address = address;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
 }
